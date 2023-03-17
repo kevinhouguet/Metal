79d848b7e32fced15e837e02940d6389773059c1
kevinhouguet <k.houguet@gmail.com>
2023-03-17 22:07:35 +0100

    add style

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const router = require('./routers');
 
 const app = express();
@@ -7,6 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(router);
 
