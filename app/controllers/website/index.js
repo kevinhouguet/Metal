c8059ed8dad60b6919d68a966f0c7f55bcd03edb
kevinhouguet <k.houguet@gmail.com>
2023-03-18 18:17:29 +0100

    update and dynam user interface

--- app/controllers/website/index.js
+++ app/controllers/website/index.js
@@ -3,11 +3,12 @@ const datamapper = require('../../models/datamapper');
 async function getHome(req, res) {
   const metals = await datamapper.getAllMetals();
 
-  await Promise.all(metals.map(async (category) => {
-    const items = await datamapper.getItemsByMetalId(category.id);
-    if (items) category.items = items;
+  await Promise.all(metals.map(async (metal) => {
+    const items = await datamapper.getItemsByMetalId(metal.id);
+    if (items) {
+      metal.items = items;
+    }
   }));
-
   res.status(200).render('home', { metals });
 }
 
