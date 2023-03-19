f1e7ec26cf1efa229fe892c1165d935325c926db
kevinhouguet <k.houguet@gmail.com>
2023-03-19 22:05:09 +0100

    :sparkles: feat: add metal

--- app/controllers/admin/index.js
+++ app/controllers/admin/index.js
@@ -1,5 +1,9 @@
-function getHome(req, res) {
-  res.status(200).render('admin');
+const datamapper = require('../../models/datamapper');
+
+async function getHome(req, res) {
+  const metals = await datamapper.getAllMetals();
+  const items = await datamapper.getAllItemsWData();
+  res.status(200).render('admin', { metals, items });
 }
 
 module.exports = {
--- app/routers/admin/index.js
+++ app/routers/admin/index.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const adminController = require('../../controllers/admin');
 const authenticationController = require('../../controllers/admin/authentificationController');
 const errorHandler = require('../../controllers/controllerErrorHandler');
+const metalController = require('../../controllers/admin/metalController');
 
 const router = new Router();
 
@@ -11,5 +12,8 @@ router.post('/signin', errorHandler(authenticationController.signin));
 router.get('/signout', authenticationController.isAuthenticated, errorHandler(authenticationController.signout));
 router.get('/profil', authenticationController.isAuthenticated, errorHandler(authenticationController.profil));
 router.patch('/profil', authenticationController.isAuthenticated, errorHandler(authenticationController.updateProfil));
+
+router.get('/addMetal', authenticationController.isAuthenticated, errorHandler(metalController.addMetalForm));
+router.post('/addMetal', authenticationController.isAuthenticated, errorHandler(metalController.addMetal));
 
 module.exports = router;
